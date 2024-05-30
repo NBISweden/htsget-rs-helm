37d541e924f1b483de74160c617f81802a9d9cca
Alex Aperis <aperisalex@gmail.com>
2024-05-30 16:15:03 +0200

    fix integration tests
    - remove certificate filepaths if tls is disabled

--- .github/integration/scripts/deploy_charts.sh
+++ .github/integration/scripts/deploy_charts.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-set -ex
+set -e
 
+if [ "$1" == "true" ]; then
+    echo "TLS enabled"
+    sed -i 's/# .*\(ticket_server_tls.key.*\)/\1/' .github/integration/scripts/values.yaml
+    sed -i 's/# .*\(ticket_server_tls.cert.*\)/\1/' .github/integration/scripts/values.yaml
+else
+    echo "TLS disabled"
+    sed -i 's/\(ticket_server_tls.key.*\)/# \1/' .github/integration/scripts/values.yaml
+    sed -i 's/\(ticket_server_tls.cert.*\)/# \1/' .github/integration/scripts/values.yaml
+fi
 
 helm install htsget charts/htsget-rs/ \
 --set tls.clusterIssuer=cert-issuer  \
